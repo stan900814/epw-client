c7e2832fc3c33d0cfa4c98ad38ea457403e812fd
stan900814 <362328861@qq.com>
2020-03-23 22:17:54 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,44 @@
+<template>
+	<view>
+		<view class="top_bar">
+			<text @tap="change(1)">正在进行</text>
+			<text @tap="change(2)">已结束</text>
+		</view>
+		<view class="main">
+		</view>
+		<view class="bottom_tabbar">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			change(e){
+				console.log(e)
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.top_bar{
+		display: flex-inline;
+		text-align: center;
+		text{
+			margin-left:66.5pt ;
+			font-size: 14pt;
+
+		}
+	}
+
+</style>
